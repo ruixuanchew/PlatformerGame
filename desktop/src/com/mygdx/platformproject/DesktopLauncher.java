9a1064fa2c394f432b0db14bc845aad95b786c9b
Oon Jun Jie Marvin <2302045@sit.singaporetech.edu.sg>
2024-02-03 16:46:12 +0800

    marvtest

--- desktop/src/com/mygdx/platformproject/DesktopLauncher.java
+++ desktop/src/com/mygdx/platformproject/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("syTest");
+		config.setTitle("marvTest");
 		new Lwjgl3Application(new GameMaster(), config);
 	}
 }
