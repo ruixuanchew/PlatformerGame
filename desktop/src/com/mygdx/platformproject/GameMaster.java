5b479bc644fa067d1466e3ca0a49e4d78f931193
toshikonoko <144001623+toshikonoko@users.noreply.github.com>
2024-02-12 17:43:44 +0800

    removed map

--- desktop/src/com/mygdx/platformproject/GameMaster.java
+++ desktop/src/com/mygdx/platformproject/GameMaster.java
@@ -11,23 +11,11 @@ import inputOutputManager.InputOutputManager;
 
 public class GameMaster extends Game {
     private InputOutputManager ioManager;
-    private SpriteBatch batch;
-    private Texture bucketImage;
-    private Rectangle player;
 
     @Override
     public void create() {
         ioManager = new InputOutputManager();
-        
-        /* for testing camera and player movement in map, can probably move to Player class */
-        batch = new SpriteBatch();
-        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-        player = new Rectangle();
-        player.x = 0;
-        player.y = 30;
-        player.width = 25;
-        player.height = 25;
-        ioManager.getMap().setPlayer(player);
+       
     }
 
     @Override
@@ -35,15 +23,6 @@ public class GameMaster extends Game {
         super.render();
         ioManager.getMap().render();
         ioManager.getSoundPlayer();
-
-        batch.begin();
-        batch.draw(bucketImage, player.x, player.y, player.width, player.height);
-        batch.end();
-
-        if (Gdx.input.isKeyPressed(Keys.LEFT)) player.x -= 200 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Keys.RIGHT)) player.x += 200 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Keys.DOWN)) player.y -= 200 * Gdx.graphics.getDeltaTime();
-        if (Gdx.input.isKeyPressed(Keys.UP)) player.y += 200 * Gdx.graphics.getDeltaTime();
     }
 
 }
