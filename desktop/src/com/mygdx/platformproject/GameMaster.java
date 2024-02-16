ce0c68908c30bde68e9bcf4e7e6c44d639e8e7d1
ngjocelyn <ngminyuanjocelyn@gmail.com>
2024-02-16 13:19:45 +0800

    added playerController and updated gamemaster to test logic

--- desktop/src/com/mygdx/platformproject/GameMaster.java
+++ desktop/src/com/mygdx/platformproject/GameMaster.java
@@ -1,35 +1,54 @@
 package com.mygdx.platformproject;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
 
 import inputOutputManager.InputOutputManager;
+import playerController.PlayerControllerManager;
+import playerController.entitytest;
 import sceneManager.SceneManager;
 
 public class GameMaster extends Game {
-    private InputOutputManager ioManager;
-    private SceneManager sceneManager;
+	private InputOutputManager ioManager;
+	private SceneManager sceneManager;
+	private PlayerControllerManager pcManager;
+	private ShapeRenderer shape;
+	private Circle circle;
+	private entitytest entity;
 
-    @Override
-    public void create() {
-        ioManager = new InputOutputManager();
-        sceneManager = new SceneManager(this);
+	@Override
+	public void create() {
+		ioManager = new InputOutputManager();
+		sceneManager = new SceneManager(this);
 		sceneManager.showStartScene();
-    }
+		shape = new ShapeRenderer();
 
-    @Override
-    public void render() {
-        super.render();
-        ioManager.Map().render();
-        ioManager.backgroundMusic();
-    }
-    @Override
-	 public void dispose() {
-	     sceneManager.dispose();
-	 }
+		// circle = new Circle(300, 300, 30);
+
+		// Initialize the PlayerControllerManager and entitytest instances
+		entity = new entitytest(300, 300, 30, 200, Color.RED);
+		pcManager = new PlayerControllerManager(entity);
+
+	}
+
+	@Override
+	public void render() {
+		super.render();
+		ioManager.Map().render();
+		ioManager.backgroundMusic();
+
+		shape.begin(ShapeRenderer.ShapeType.Filled); // Add the shape type before drawing the shape
+		entity.draw(shape);
+		shape.end();
+
+		pcManager.move();
+	}
+
+	@Override
+	public void dispose() {
+		sceneManager.dispose();
+	}
 
 }
