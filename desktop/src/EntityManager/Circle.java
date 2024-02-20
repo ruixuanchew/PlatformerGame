64c7c2d4acf555beb431620404af64719da31126
chuashingying <87071276+chuashingying@users.noreply.github.com>
2024-02-21 00:02:32 +0800

    Update Circle.java
    commented out circle class

--- desktop/src/EntityManager/Circle.java
+++ desktop/src/EntityManager/Circle.java
@@ -1,47 +1,47 @@
-package EntityManager;
+// package EntityManager;
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector2;
+// import com.badlogic.gdx.graphics.Color;
+// import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+// import com.badlogic.gdx.math.Vector2;
 
-public class Circle extends Entity {
-	private float radius;
+// public class Circle extends Entity {
+// 	private float radius;
 
-	// default
-	public Circle() {
+// 	// default
+// 	public Circle() {
 
-	}
+// 	}
 
-	// parameterized
-	public Circle(float radius, float posX, float posY, float speed, Color colour, boolean isPlayer) {
-		super(posX, posY, speed, colour, isPlayer);
-		this.radius = radius;
-	}
+// 	// parameterized
+// 	public Circle(float radius, float posX, float posY, float speed, Color colour, boolean isPlayer) {
+// 		super(posX, posY, speed, colour, isPlayer);
+// 		this.radius = radius;
+// 	}
 
-	public Circle(float radius, float posX, float posY, float speed, Color colour, boolean isPlayer, Vector2 velocity) {
-		super(posX, posY, speed, colour, isPlayer, velocity);
-		this.radius = radius;
-	}
+// 	public Circle(float radius, float posX, float posY, float speed, Color colour, boolean isPlayer, Vector2 velocity) {
+// 		super(posX, posY, speed, colour, isPlayer, velocity);
+// 		this.radius = radius;
+// 	}
 
-	public float getRadius() {
-		return radius;
-	}
+// 	public float getRadius() {
+// 		return radius;
+// 	}
 
-	public void setRadius(float r) {
-		this.radius = r;
-	}
+// 	public void setRadius(float r) {
+// 		this.radius = r;
+// 	}
 
-	@Override
-	public void draw(ShapeRenderer shape) {
-		// Set the drawing color
-		shape.setColor(this.getColour());
+// 	@Override
+// 	public void draw(ShapeRenderer shape) {
+// 		// Set the drawing color
+// 		shape.setColor(this.getColour());
 
-		// Draw the circle using the Circle's position and radius
-		shape.circle(this.getPosX(), this.getPosY(), this.radius);
-	}
+// 		// Draw the circle using the Circle's position and radius
+// 		shape.circle(this.getPosX(), this.getPosY(), this.radius);
+// 	}
 
-	@Override
-	public void movement() {
+// 	@Override
+// 	public void movement() {
 //		if (Gdx.input.isKeyPressed(Keys.UP))
 //		{
 //			float y = this.getPosY() + this.getSpeed() * Gdx.graphics.getDeltaTime();
@@ -53,21 +53,21 @@ public class Circle extends Entity {
 //			float y = this.getPosY() - this.getSpeed() * Gdx.graphics.getDeltaTime();
 //			this.setPosY(y);
 //		}
-	}
+// 	}
 
-	@Override
-	public void update() {
+// 	@Override
+// 	public void update() {
 
-	}
+// 	}
 
-	@Override
-	public void moveAIControlled() {
+// 	@Override
+// 	public void moveAIControlled() {
 		
-	}
+// 	}
 
-	@Override
-	public void moverUserControlled() {
-		this.movement();
-	}
+// 	@Override
+// 	public void moverUserControlled() {
+// 		this.movement();
+// 	}
 
-}
+// }
